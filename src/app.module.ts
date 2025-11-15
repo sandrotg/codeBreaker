8b9a8f129252d80ea4ef313f6768156b55d17af0
Key <mkeiver@uninorte.edu.co>
2025-11-15 15:32:59 -0500

    app module
    added access to jobsModule and WorkerModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ChallengeModule } from './presentation/challenges/modules/challenge.module';
 import { TestCaseModule } from './presentation/challenges/modules/testCase.module';
 import { SubmissionModule } from './presentation/submissions/submission.module';
+import { JobsModule } from './presentation/jobs/modules/jobs.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
   imports: [
@@ -11,7 +13,9 @@ import { SubmissionModule } from './presentation/submissions/submission.module';
     }),
   ChallengeModule,
   TestCaseModule,
-  SubmissionModule
+  SubmissionModule,
+  JobsModule,
+  WorkerModule
   ],
 })
 export class AppModule {}
